9d42305254f4b25a2950065531a2ca1492ddde1f
Joey Elliott <joey.w.elliott@gmail.com>
2024-11-24 14:05:37 -0600

    clean up

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
@@ -11,25 +12,28 @@ func main() {
 	app.Route("/", func() app.Composer { return NewHome() })
 	app.RunWhenOnBrowser()
 
-	if err := app.GenerateStaticWebsite("./build", &app.Handler{
-		Name:        "honorable sudoku",
-		Description: "simply sudoku",
-		Resources:   app.GitHubPages("honorable_sudoku"),
-		Styles: []string{
-			"/web/sudoku.css",
-		},
-	}); err != nil {
-		log.Fatal(err)
+	if *flag.Bool("generate", false, "Generates the PWA instead of hosting") {
+		if err := app.GenerateStaticWebsite(".", &app.Handler{
+			Name:        "honorable sudoku",
+			Description: "simply sudoku",
+			Resources:   app.GitHubPages("honorable_sudoku"),
+			Styles: []string{
+				"./web/sudoku.css",
+			},
+		}); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		http.Handle("/", &app.Handler{
+			Name:        "Home",
+			Description: "The home page",
+			Styles: []string{
+				"./web/sudoku.css",
+			},
+		})
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	http.Handle("/", &app.Handler{
-		Name:        "Home",
-		Description: "The home page",
-		Styles: []string{
-			"/web/sudoku.css",
-		},
-	})
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
 }
